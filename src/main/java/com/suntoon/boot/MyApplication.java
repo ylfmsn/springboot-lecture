7bc61a10b9f998a9d86b5c98fa4673f6e5cbdcc9
ylfmsn <yuelifeng0415@aliyun.com>
2020-02-15 20:43:59 +0800

    update

--- src/main/java/com/suntoon/boot/MyApplication.java
+++ src/main/java/com/suntoon/boot/MyApplication.java
@@ -3,10 +3,14 @@ package com.suntoon.boot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * JDWP: Java Debug Wire Protocol, Java调试协议
+ */
 @SpringBootApplication
 public class MyApplication {
 
     public static void main(String[] args) {
+        System.out.println(MyApplication.class.getClassLoader());
         SpringApplication.run(MyApplication.class, args);
     }
 }
